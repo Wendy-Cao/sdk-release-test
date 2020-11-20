652e8da0975bd75dc0779a37f8abba6b67b498ec
wendy <2226441815@qq.com>
2020-11-20 15:00:52 +0800

    添加projects

---
+++ template_projects/img_rt1050_debug/user_code/main.c
@@ -0,0 +1,32 @@
+/*******************************************************************************
+*                                 AWorks
+*                       ---------------------------
+*                       innovating embedded platform
+*
+* Copyright (c) 2001-2012 Guangzhou ZHIYUAN Electronics Co., Ltd.
+* All rights reserved.
+*
+* Contact information:
+* web site:    http://www.zlg.cn/
+*******************************************************************************/
+
+#include "aworks.h"
+#include "aw_vdebug.h"
+#include "aw_led.h"
+#include "aw_delay.h"
+
+#define LED 0
+
+int aw_main()
+{
+    aw_kprintf("\r\nApplication Start. \r\n");
+
+    while (1) {
+        aw_led_toggle(LED);
+        aw_mdelay(500);
+    }
+
+    return 0;
+}
+
+/* end of file*/
